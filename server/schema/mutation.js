5e208b58353d1e7095dcf5d51b862a94375efb89
halim <haleemyoosuph@gmail.com>
2020-12-02 03:10:40 +0100

    chore: add signup mutation

---
+++ server/schema/mutation.js
@@ -0,0 +1,22 @@
+const UserType = require('./types/user');
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString } = graphql;
+const authService = require('../services/auth');
+
+const mutation = new GraphQLObjectType({
+  type: 'Mutation',
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(src, { email, password }, req) {
+        return authService.signup({ email, password, req });
+      },
+    },
+  },
+});
+
+module.exports = mutation;
