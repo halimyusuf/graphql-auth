19decc4bcbb332a4f2292d2f13cc7a23b9b0fc94
halim <haleemyoosuph@gmail.com>
2021-01-03 01:04:10 +0100

    chore: add login and signup route

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router';
+import { Router, Route, Switch } from 'react-router';
+import history from './CustomHistory';
 import Home from './Home';
+import Login from './Login';
+import Signup from './Signup';
 
 const App = () => {
   return (
     <div>
-      <Router>
-        <Route path='/'>
+      <Router history={history}>
+        <Route exact path='/'>
           <Home />
+        </Route>
+
+        <Route exact path='/login'>
+          <Login />
+        </Route>
+
+        <Route exact path='/signup'>
+          <Signup />
         </Route>
       </Router>
     </div>
